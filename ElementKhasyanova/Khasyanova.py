d71231ddcd36ffd27d91ecf377a2f7fa92846835
Ильмира <khasyanova.ilmira@bk.ru>
2024-12-09 13:22:38 +0500

    Update Khasyanova.py

--- ElementKhasyanova/Khasyanova.py
+++ ElementKhasyanova/Khasyanova.py
@@ -1,31 +1,21 @@
-import math
-
-class Shape:
-
-    def __init__(self):
-        self._shape_type = "Geometric Shape" 
-    @staticmethod
-    def about():
-        print("Программа расчета объема геометрических фигур.")
-        print("Разработано командой из трех человек, а именно Сираев Т.В. Хасянова И.И. Фатихов Р.С.")
-    def calculate_volume(self):
-        raise NotImplementedError("Этот метод должен быть переопределен в дочернем классе.")
-class Sphere(Shape):
-
-    def init(self, radius):
-        super().init()  
-        self._radius = radius  
-        self._shape_type = "Sphere"  
-
+class ElementKhasyanova:
+    def init(self, name='Хром', symbol='Cr', number='24'):
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
     @property
-    def radius(self):
-        return self._radius
-
-    @radius.setter
-    def radius(self, value):
-        if value <= 0:
-            raise ValueError("Радиус должен быть положительным числом.")
-        self._radius = value
-
-    def calculate_volume(self):
-        return (4 / 3) * math.pi * self._radius**3
+    def name(self):
+        return self.__name
+    @property
+    def symbol(self):
+        return self.__symbol
+    @property
+    def number(self):
+        return self.__number
+    def dump(self):
+        print(f"Имя элемента: {self.name}")
+        print(f"Символ: {self.symbol}")
+        print(f"Атомный номер: {self.number}")
+if name == "main":
+    element = ElementKhasyanova()
+    element.dump()
