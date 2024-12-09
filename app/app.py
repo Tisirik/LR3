0f7bab125a83a812eafd0802f581f988cfe1f324
Radim-Fatihov <fatihovradim@gmail.com>
2024-12-09 13:05:01 +0500

    Add files via upload

--- app/app.py
+++ app/app.py
@@ -1,12 +1,11 @@
-import math
+if name == "main":
+    Shape.about()
 
-class Shape:
+    try:
+        radius = float(input("Введите радиус шара: "))
+        sphere = Sphere(radius)
 
-    def __init__(self):
-        self._shape_type = "Geometric Shape" 
-    @staticmethod
-    def about():
-        print("Программа расчета объема геометрических фигур.")
-        print("Разработано командой из трех человек, а именно Сираев Т.В. Хасянова И.И. Фатихов Р.С.")
-    def calculate_volume(self):
-        raise NotImplementedError("Этот метод должен быть переопределен в дочернем классе.")
+        volume = sphere.calculate_volume()
+        print(f"Объем шара с радиусом {sphere.radius} равен {volume:.2f}")
+    except ValueError as e:
+        print(f"Ошибка: {e}")
