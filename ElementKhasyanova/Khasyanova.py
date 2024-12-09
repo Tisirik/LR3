e0220612bb9007f944e24e289d56b21f9b00cab7
Ильмира <khasyanova.ilmira@bk.ru>
2024-12-09 12:57:26 +0500

    Сделана вторая часть задания

--- ElementKhasyanova/Khasyanova.py
+++ ElementKhasyanova/Khasyanova.py
@@ -1,4 +1,14 @@
-class ElementKhasyanova():
-    name = 'Хром'
-    symbol = 'Cr'
-    number = '24'
+class ElementKhasyanova:
+    def init(self, name='Хром', symbol='Cr', number='24'):
+        self.name = name
+        self.symbol = symbol
+        self.number = number
+
+    def dump(self):
+        print(f"Имя элемента: {self.name}")
+        print(f"Символ: {self.symbol}")
+        print(f"Атомный номер: {self.number}")
+
+if name == "main":
+    element = ElementKhasyanova()
+    element.dump()
