c212ebfca24aab93dc93aacf716c6c4e785955d8
Radim-Fatihov <fatihovradim@gmail.com>
2024-12-09 12:59:16 +0500

    Выполнил третью часть лабораторной работы

--- ElementFatihov/Fatihov.py
+++ ElementFatihov/Fatihov.py
@@ -1,14 +1,21 @@
 class ElementFatihov:
     def __init__(self, name='Титан', symbol='Ti', number='22'):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
-
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def symbol(self):
+        return self.__symbol
+    @property
+    def number(self):
+        return self.__number
     def dump(self):
         print(f"Имя элемента: {self.name}")
         print(f"Символ: {self.symbol}")
         print(f"Атомный номер: {self.number}")
-
 if __name__ == "__main__":
     element = ElementFatihov()
     element.dump()
