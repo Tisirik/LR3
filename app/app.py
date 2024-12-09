cfbb27419b3dcae811d3063fa4a91e4290d8e88a
Ильмира <khasyanova.ilmira@bk.ru>
2024-12-09 13:04:26 +0500

    Вторая часть кода

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
-import math
+class Sphere(Shape):
 
-class Shape:
+    def init(self, radius):
+        super().init()  
+        self._radius = radius  
+        self._shape_type = "Sphere"  
 
-    def __init__(self):
-        self._shape_type = "Geometric Shape" 
-    @staticmethod
-    def about():
-        print("Программа расчета объема геометрических фигур.")
-        print("Разработано командой из трех человек, а именно Сираев Т.В. Хасянова И.И. Фатихов Р.С.")
+    @property
+    def radius(self):
+        return self._radius
+
+    @radius.setter
+    def radius(self, value):
+        if value <= 0:
+            raise ValueError("Радиус должен быть положительным числом.")
+        self._radius = value
+
     def calculate_volume(self):
-        raise NotImplementedError("Этот метод должен быть переопределен в дочернем классе.")
+        return (4 / 3) * math.pi * self._radius**3
