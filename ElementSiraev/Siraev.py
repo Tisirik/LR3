c52edb5268b2ffa07dcb832db63d4f0c081038c8
Tisirik <timik.siraev@gmail.com>
2024-12-09 12:56:33 +0500

    выполнена 3-я часть задания

--- ElementSiraev/Siraev.py
+++ ElementSiraev/Siraev.py
@@ -1,14 +1,21 @@
 class ElementSiraev:
     def __init__(self, name='Кальций', symbol='Ca', number='20'):
-        self.name = name
-        self.symbol = symbol
-        self.number = number
-
+        self.__name = name
+        self.__symbol = symbol
+        self.__number = number
+    @property
+    def name(self):
+        return self.__name
+    @property
+    def symbol(self):
+        return self.__symbol
+    @property
+    def number(self):
+        return self.__number
     def dump(self):
         print(f"Имя элемента: {self.name}")
         print(f"Символ: {self.symbol}")
         print(f"Атомный номер: {self.number}")
-
 if __name__ == "__main__":
     element = ElementSiraev()
     element.dump()
